e9e16cf92443e7d01f65d638dca579261bcbfe21
natansevero <natanseverolp@gmail.com>
2017-02-05 13:46:37 -0200

    adicionando rota de criar usuário server-side

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,9 @@
 const Users = require('./../models/users');
 
 const UsersController = {
+  create: (req, res) => {
+    
+  },
   retrieve: (req, res) => {
     Users.retrieve((err, result) => {
       if(err) return console.log('Error: ', err);
--- server/models/users.js
+++ server/models/users.js
@@ -2,6 +2,12 @@ const pg = require('pg');
 const conString = 'postgres://postgres:12345@localhost:5432/GoQuestion';
 
 const Users = {
+  create: (callback) => {
+    pq.connect(conString, (err, client) => {
+      if(err) return console.log("Error Create UserModel: ", err);
+      client.query("", callback);
+    });
+  },
   retrieve: (callback) => {
     pq.connect(conString, (err, client) => {
       if(err) return console.log("Error: ", err);
