51c95bd00e225c89643d8c1c6789e9877a9dbb3b
natansevero <natanseverolp@gmail.com>
2017-02-06 12:46:04 -0200

    Configurando rota de Users

--- server/models/users.js
+++ server/models/users.js
@@ -2,16 +2,26 @@ const pg = require('pg');
 const conString = 'postgres://postgres:12345@localhost:5432/GoQuestion';
 
 const Users = {
-  create: (callback) => {
-    pq.connect(conString, (err, client) => {
+  create: (user, callback) => {
+    console.log(user.user);
+    pg.connect(conString, (err, client) => {
       if(err) return console.log("Error Create UserModel: ", err);
-      client.query("", callback);
+      client.query(
+        `insert into usuario(usuario, sexo, idade, renda, escolaridade, localizacao, email, senha, tipo)
+        values ('${user.nome}', '${user.sexo}', ${user.idade}, ${user.renda}, '${user.escolaridade}',
+        ST_GeomFromText('POINT(${user.localizacao.lat} ${user.localizacao.lng})'), '${user.email}',
+        '${user.senha}', '${user.tipo}')`,
+        callback
+      );
     });
   },
   retrieve: (callback) => {
-    pq.connect(conString, (err, client) => {
+    pg.connect(conString, (err, client) => {
       if(err) return console.log("Error: ", err);
-      client.query("", callback);
+      client.query(
+        `select usuario, sexo, idade, renda, escolaridade, st_AsEwkt(localizacao), email, senha, tipo
+        from usuario`,
+        callback);
     });
   }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const users = require('./../controllers/users');
 
+router.post('/create', users.create);
 router.get('/', users.retrieve);
-router.post('/', users.create);
 
 module.exports = router;
