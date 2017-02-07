3fd0bacd429af5b5dfc4a18a9bd107bf9c6c244b
natansevero <natanseverolp@gmail.com>
2017-02-07 12:42:04 -0200

    Adicionando middleware de auth e modulo de config de jwt

---
+++ server/middlewares/auth.js
@@ -0,0 +1,12 @@
+module.exports = (req, res, next) => {
+  var bearerToken;
+  var bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+        var bearer = bearerHeader.split(" ");
+        bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.send(403);
+    }
+}
